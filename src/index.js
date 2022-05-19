40212bc22d637fd6c1a325b8ed850ee2b3023c4f
irinaog <goncharirina08@gmail.com>
2022-05-19 10:46:44 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -11,23 +11,27 @@ const refs = {
     list: document.querySelector('.country-list'),
     block: document.querySelector('.country-info'),
 };
-input.addEventListener('input', debounce (onSearch, DEBOUNCE_DELAY));
+
+const country = input.value.trim();
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+// input.addEventListener('input', onSearch(country));
 // input.addEventListener('input', onSearch);
 
 function onSearch() {
     
-    const country = input.value;
+    const country = input.value.trim();
     if (country !== "") {
+        // debounce(fetchCountries(`${country}`), DEBOUNCE_DELAY);
         fetchCountries(`${country}`);
-        markupCountry(`${country}`);
-        refs.list.innerHTML = ' ';
+         refs.list.innerHTML = ' ';
         refs.block.innerHTML = ' ';
+        markupCountry(`${country}`);
+       
     };
 };
 
 function markupCountry(country) {
     fetchCountries(country).then(data => {
-        console.dir(data[0].flags.svg);
         if (data.length >= 10) {
             console.log("more then 10")
             Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
@@ -35,21 +39,26 @@ function markupCountry(country) {
         };
         if (data.length >= 2 && data.length <= 10) {
             data.map(country => {
-                refs.list.insertAdjacentHTML('beforeend', `<li><img class = "flag" src ="${country.flags.svg}"><span>${country.name.official}</span></li>`);
+                refs.list.insertAdjacentHTML('beforeend', `<li class="country-list__item"><img class = "flag" src ="${country.flags.svg}"><span class="country-list__name">${country.name.official}</span></li>`);
             });
             return
         };
         if (data.length === 1) {
+            refs.list.innerHTML = ' ';
             const language =Object.values(data[0].languages);
             refs.block.insertAdjacentHTML('beforeend', `
             <img src="${data[0].flags.svg}" class ="bigflag">
             <h2 class = "country-info__name">${data[0].name.official}</h2>
             <ul class="country">
-            <li class="country__item"><b>Capital:</b>${data[0].capital}</span></li>
-            <li class="country__item"><b>Population:</b>${data[0].population}</span></li>
-            <li class="country__item"><b>Languages:</b>${language}</span></li>
+            <li class="country__item"><b>Capital:</b>&nbsp ${data[0].capital}</span></li>
+            <li class="country__item"><b>Population:</b> &nbsp ${data[0].population}</span></li>
+            <li class="country__item"><b>Languages:</b> &nbsp ${language}</span></li>
             </ul>`);
-        };
+            return
+        }
+        else {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+        }
 
     });
 };
